97dd3b2251b47402ed0808b2fa549a12abe80fad
Lee Suhyeon <suhyeon3868@naver.com>
2020-11-20 02:43:51 +0900

    modified algorithm

--- algorithm/script/al_subscriber.py
+++ algorithm/script/al_subscriber.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 import rospy
 from common_msgs.msg import cmn
+from common_msgs.srv import AddTwoNum, AddTwoNumResponse
 
 def callback(msg):
-    print "subscribe:", msg.timestamp.secs%100, msg.point.x, msg.point.y, msg.point.z
+   print("Time: ",msg.timestampsecs%100)
 
-rospy.init_node('algorithm_subscriber')
-sub = rospy.Subscriber('sensor_msg', cmn, callback)
+def service_callback(request):
+    response = AddTwoNumResponse(sum=request.a + request.b)
+    print "request data:", request.a, request.b, ", response:", response.sum
+    return response
+
+
+rospy.init_node('service_server')
+service = rospy.Service('add_two_number', AddTwoNum, service_callback)
 rospy.spin()
