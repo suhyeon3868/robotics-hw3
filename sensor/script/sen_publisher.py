eda1a4ba123b2be621c5a9a3760a4f22c473fb9f
Lee Suhyeon <suhyeon3868@naver.com>
2020-11-20 02:48:32 +0900

    modified common_msgs and sen_publisher.py

--- sensor/script/sen_publisher.py
+++ sensor/script/sen_publisher.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python
 
 import rospy
-from geometry_msgs.msg import Point
+import random
 from common_msgs.msg import cmn
+from common_msgs.srv import AddTwoNum, AddTwoNumRequest
 
-rospy.init_node('sensor_publisher')
-pub = rospy.Publisher('sensor_msg', cmn, queue_size=1)
-msg = cmn()
+rospy.init_node('service_client')
+rospy.wait_for_service('add_two_number')
+requester = rospy.ServiceProxy('add_two_number', AddTwoNum)
+pub = rospy.Publisher('cmn', cmn, queue_size=1)
+print "requester type:", type(requester), ", callable?", callable(requester)
+msg=cmn()
 rate = rospy.Rate(1)
 while not rospy.is_shutdown():
-    msg.timestamp = rospy.get_rostime()
-    second = msg.timestamp.secs
-    msg.point = Point(x=second%4, y=second%7, z=second%5)
+    msg.timestamp=rospy.get_rostime()
+    sec=msg.timestamp.secs%100
     pub.publish(msg)
-    print "publish:", msg.timestamp.secs%100, msg.point.x, msg.point.y, msg.point.z
+    if sec % 10 == 0:
+        req = AddTwoNumRequest(a=random.randint(1,50),b=random.randint(1,50))
+        res = requester(req)
+        print sec, "request:", req.a, req.b, "response:", res.sum
     rate.sleep()
+
